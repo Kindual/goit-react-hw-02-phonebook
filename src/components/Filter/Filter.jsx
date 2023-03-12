baf81d61bfe16ed1b53c21d2436a877063ac195d
Kindual <110603179+Kindual@users.noreply.github.com>
2023-03-12 21:26:56 +0200

    complete

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,12 +5,15 @@ export default class Filter extends Component {
     render() {
         const { updateFilterState, filter } = this.props;
         return (
+            <>
+            <p className={css.label}>Find contacts by name</p>
             <input
                 type="text"
                 value={filter}
                 className={css.filterInput}
                 onChange={e => updateFilterState(e.target.value)}
             />
+            </>
         )
     }
 }
